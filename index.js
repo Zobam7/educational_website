174c3d6c55999183db28b2ff356f6bc6691331a5
Zobam7 <zobamokoli7@gmail.com>
2022-04-15 01:57:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//NAVBAR COOLOR CHANGE
+
+
+window.addEventListener("scroll", ()=> {
+    document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
+})
+
+
+//SHOW/HIDE FAQ
+
+const faqs = document.querySelectorAll(".faq")
+
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", ()=> {
+        faq.classList.toggle("open")
+
+
+        const icon = faq.querySelector(".faq__icon i")
+        if(icon.className === "uil uil-plus"){
+            icon.className =  "uil uil-minus"
+        }else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
+
+const menu = document.querySelector(".nav__menu")
+const menubtn = document.querySelector("#open-menu-btn")
+const closebtn = document.querySelector("#close-menu-btn")
+
+menubtn.addEventListener("click", () => {
+    menu.style.display = "flex"
+    closebtn.style.display = "inline-block"
+    menubtn.style.display = "none"
+})
+
+const closeNav = () => {
+    menu.style.display = "none"
+    closebtn.style.display = "none"
+    menubtn.style.display = "inline-block"
+}
+
+closebtn.addEventListener("click", closeNav)
